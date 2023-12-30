3feaa960736bcee2746adf263d376446648beb66
Vyacheslav Bazilevich <bazilevich.v@yandex.ru>
2023-12-30 12:46:45 +0800

    initial commit

---
+++ Main.kt
@@ -0,0 +1,17 @@
+package ru.netology
+
+
+fun main() {
+    val likes = 41
+    val endCount = " людям"
+    val endCount1 = " человеку"
+    if(likes === 21 || likes === 31 || likes === 41 || likes === 51 || likes === 61 || likes === 71 || likes === 81){
+        println(endCount1)
+    } else{
+        println(endCount)
+    }
+
+
+
+
+}
